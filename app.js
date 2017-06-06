17e48be1b46bbe6925f6d16b5181ab6d2288d583
Elvis Chan <choectt@gmail.com>
2017-06-05 23:29:11 -0700

    Express Router refactor

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
+
+// Article model
 const Article = require('./models/article');
 
 mongoose.connect('mongodb://localhost/nodekb');
@@ -78,98 +80,10 @@ app.get('/', function (req, res) {
   });
 });
 
-// get single article
-app.get('/article/:id', function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render('article', {
-      article: article
-    });
-  });
-});
-
-// new article form
-app.get('/articles/add', function(req, res){
-  res.render('add_article', {
-    title: 'Add Article'
-  });
-});
-
-// submit new article 
-app.post('/articles/add', function(req, res){
-  // Express validator
-  req.checkBody('title', 'Title is required').notEmpty();
-  req.checkBody('author', 'Author is required').notEmpty();
-  req.checkBody('body', 'Body is required').notEmpty();
-  
-  // Get errors
-  let errors = req.validationErrors();
-
-  if(errors){
-    res.render('add_article', {
-      title: 'Add Article',
-      errors: errors
-    });
-  } else {
-    let article = new Article();
-    article.title = req.body.title;
-    article.author = req.body.author;
-    article.body = req.body.body;
-
-    article.save(function(err){
-      if(err) {
-        console.error(err);
-        return;
-      } else {
-        req.flash('success', 'Article Added');
-        res.redirect('/');
-      }
-    });
-  }
-});
-
-// load edit form
-app.get('/article/edit/:id', function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render('edit_article', {
-      title: 'Edit Article',
-      article: article
-    });
-  });
-});
-
-// update submit new article 
-app.post('/articles/edit/:id', function(req, res){
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = {_id: req.params.id};
-
-  Article.update(query, article, function(err){
-    if(err) {
-      console.error(err);
-      return;
-    } else {
-      req.flash('success', 'Article Updated');
-      res.redirect('/');
-    }
-  })
-});
-
-app.delete('/article/:id', function(req, res){
-  let query = {_id: req.params.id};
-
-  Article.remove(query, function(err){
-    if(err) {
-      console.error(err);
-      return;
-    } else {
-      req.flash('success', 'Article Deleted')
-      res.send('Success');
-    }
-  });
-});
+// Route Files
+let articles = require('./routes/articles');
+// Any routes that goes to '/articles' will go to the 'articles.js' file in route
+app.use('/articles', articles);
 
 app.listen(3000, function(){
   console.log(`Server started on port 3000`);
